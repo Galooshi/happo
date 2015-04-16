a110bd05273f5b952d77b15b644421ebee2276f9
Henric Trotzig <henric.trotzig@causes.com>
2015-04-16 10:32:29 +0200

    Remove DOM manipulation when rendering example
    Before, we would take the rendered element and attach it to the
    `#likadan-output` div. This approach had a few problems:
    
     - React don't want its elements to be moved around in the DOM. This
       project is definitely going to be used with React. One issue was that
       we didn't have a way to unmount rendered components.
     - It hurts performance slightly to move stuff around in the DOM.
    
    I've changed this to instead allow examples to be rendered anywhere in
    the DOM themselves. We remember the rendered element and unmount it (in
    case of React) or remove it from the DOM (plain JS).
    
    I've been trying to stay clear of React dependencies. This commit
    makes it a bit more tied to React, but not completely. I might look at
    an alternative solution to unmounting for instance. We could allow
    before and after hooks to be defined. That way it could be up to the
    project itself to unmount.

--- example-project/example.js
+++ example-project/example.js
@@ -1,18 +1,21 @@
 likadan.define('foo', function() {
   var elem = document.createElement('div');
   elem.setAttribute('class', 'foo');
-  elem.innerHTML = 'Text inside';
+  elem.innerHTML = 'Texts insides';
+  document.body.appendChild(elem);
   return elem;
 });
 
 likadan.define('hallooo', function() {
   var elem = document.createElement('span');
   elem.innerHTML = 'Hioi!<br>Hello';
+  document.body.appendChild(elem);
   return elem;
 }, [320, 1024]);
 
 likadan.define('bar', function() {
   var elem = document.createElement('span');
   elem.innerHTML = 'go bars!<br>bars';
+  document.body.appendChild(elem);
   return elem;
 });
