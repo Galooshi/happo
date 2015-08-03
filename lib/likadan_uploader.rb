f2cd7857bbe36cc8ea924b74ba21b1edc7af6425
Joe Lencioni <joe.lencioni@brigade.com>
2015-08-03 08:49:55 -0700

    Avoid creating multiple S3 buckets
    We started seeing the following error when likadan runs in CI:
    
    > You have attempted to create more buckets than allowed
    > (S3::Error::TooManyBuckets)
    
    I think we can avoid this by only creating one bucket instead of a new
    bucket every time likadan runs. I simply moved the random bucket name to
    be a random file name, and created a non-random bucket name.
    
    I decided to switch from SecureRandom.hex to SecureRandom.uuid because
    uuid seems more appropriate for this type of thing.
    
    I considered making the filename for the diffs just be the MD5 hash of
    the file, which would avoid us from uploading duplicate data if the two
    files happened to be the same. However, I didn't think the little bit of
    added complexity of finding if the file already exists was worth the
    small gain of no duplicates at this time.

--- lib/likadan_uploader.rb
+++ lib/likadan_uploader.rb
@@ -3,19 +3,18 @@ require 's3'
 require 'securerandom'
 
 class LikadanUploader
-  def initialize()
+  BUCKET_NAME = 'likadan-diffs'
+
+  def initialize
     @s3_access_key_id = LikadanUtils.config['s3_access_key_id']
     @s3_secret_access_key = LikadanUtils.config['s3_secret_access_key']
   end
 
   def upload_diffs
-    service = S3::Service.new(access_key_id: @s3_access_key_id,
-                              secret_access_key: @s3_secret_access_key)
     current_snapshots = LikadanUtils.current_snapshots
     return [] if current_snapshots[:diffs].empty?
 
-    bucket = service.buckets.build(SecureRandom.hex)
-    bucket.save(location: :us)
+    bucket = find_or_build_bucket
 
     diff_images = current_snapshots[:diffs].map do |diff|
       image = bucket.objects.build("#{diff[:name]}_#{diff[:viewport]}.png")
@@ -26,7 +25,7 @@ class LikadanUploader
       diff
     end
 
-    html = bucket.objects.build('likadan-diffs.html')
+    html = bucket.objects.build("#{SecureRandom.uuid}.html")
     html.content =
       ERB.new(
         File.read(File.expand_path(
@@ -35,5 +34,20 @@ class LikadanUploader
     html.content_type = 'text/html'
     html.save
     html.url
+  end
+
+  private
+
+  def find_or_build_bucket
+    service = S3::Service.new(access_key_id: @s3_access_key_id,
+                              secret_access_key: @s3_secret_access_key)
+    bucket = service.buckets.find(BUCKET_NAME)
+
+    if bucket.nil?
+      bucket = service.buckets.build(BUCKET_NAME)
+      bucket.save(location: :us)
+    end
+
+    bucket
   end
 end
