edaa59229569399143ccdaad6b438826d0b25fa8
Henric Trotzig <henric.trotzig@brigade.com>
2016-08-28 22:55:11 -0700

    WIP: break apart diff image

--- lib/happo/public/HappoDiffs.jsx
+++ lib/happo/public/HappoDiffs.jsx
@@ -4,22 +4,30 @@ const imageObjectStructure = {
   description: PropTypes.string.isRequired,
   viewport: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
+  beforeUrl: PropTypes.string,
+  afterUrl: PropTypes.string,
 };
 
 function imageSlug(image) {
   return btoa(image.description +  image.viewport);
 }
 
-function renderImage(image) {
+function renderImageHeading(image) {
+  return (
+    <h3 id={imageSlug(image)}>
+      <a className='anchored' href={`#${imageSlug(image)}`}>
+        {image.description}
+        {' @ '}
+        {image.viewport}
+      </a>
+    </h3>
+  );
+}
+
+function renderNewImage(image) {
   return (
     <div>
-      <h3 id={imageSlug(image)}>
-        <a className='anchored' href={`#${imageSlug(image)}`}>
-          {image.description}
-          {' @ '}
-          {image.viewport}
-        </a>
-      </h3>
+      {renderImageHeading(image)}
       <img src={image.url} />
     </div>
   );
@@ -38,7 +46,12 @@ function renderDiffImages(diffImages) {
         </a>
       </h2>
 
-      {diffImages.map(renderImage)}
+      {diffImages.map((image, i) => (
+        <HappoDiff
+          key={i}
+          image={image}
+        />
+      ))}
     </div>
   );
 }
@@ -56,10 +69,31 @@ function renderNewImages(newImages) {
         </a>
       </h2>
 
-      {newImages.map(renderImage)}
+      {newImages.map(renderNewImage)}
     </div>
   );
 }
+
+const HappoDiff = React.createClass({
+  propTypes: {
+    image: PropTypes.shape(imageObjectStructure),
+  },
+
+  render() {
+    const {
+      image,
+    } = this.props;
+
+    return (
+      <div>
+        {renderImageHeading(image)}
+        <img src={image.beforeUrl} />
+        <img src={image.url} />
+        <img src={image.afterUrl} />
+      </div>
+    );
+  }
+});
 
 window.HappoDiffs = React.createClass({
   propTypes: {
