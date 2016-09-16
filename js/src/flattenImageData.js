326875e6812d3e3a2bf38687494af957ea25e220
Henric Trotzig <henric.trotzig@brigade.com>
2016-09-16 15:47:00 +0200

    Optimize flattenImageData
    By not constructing the array twice, and using reference by index
    instead of pushing, we can reduce the time it takes to flatten a 2d
    array down from ~100ms on a medium-sized image to less than 5ms.

--- js/src/flattenImageData.js
+++ js/src/flattenImageData.js
@@ -3,11 +3,16 @@
  * @return {Uint8ClampedArray}
  */
 export default function flattenImageData(imageData) {
-  const result = [];
-  imageData.forEach((row) => {
-    row.forEach((pixel) => {
-      result.push(...pixel);
+  const width = imageData[0].length;
+  const result = new Uint8ClampedArray(imageData.length * width * 4);
+  imageData.forEach((row, y) => {
+    row.forEach((pixel, x) => {
+      const index = (y * width * 4) + (x * 4);
+      result[index] = pixel[0];
+      result[index + 1] = pixel[1];
+      result[index + 2] = pixel[2];
+      result[index + 3] = pixel[3];
     });
   });
-  return Uint8ClampedArray.from(result);
+  return result;
 }
