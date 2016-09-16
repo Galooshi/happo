27155ad90d085ba9b21702d0fa103cf5329e9109
Henric Trotzig <henric.trotzig@brigade.com>
2016-09-16 06:55:58 +0200

    Restructure <LCSDiff>
    Instead of having to deal with undefined values in state, we can use
    Promise.all to wait for both images to resolve until we compute the
    diffs. This led to a simpler setup.
    
    Promise.all will resolve promises in parallell, but the order of the
    result array will be the same as the order on which promises were
    invoked.

--- js/src/LCSDiff.jsx
+++ js/src/LCSDiff.jsx
@@ -9,52 +9,53 @@ const ImageDiffWorker =
 export default class LCSDiff extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      previousData: null,
-      currentData: null,
-    };
+    this.state = {};
   }
 
   componentDidMount() {
     const { previous, current } = this.props;
 
-    getImageData(previous).then((previousData) => {
-      this.setState({ previousData });
-    });
-
-    getImageData(current).then((currentData) => {
-      this.setState({ currentData });
+    Promise.all([
+      getImageData(previous),
+      getImageData(current),
+    ]).then((result) => {
+      this.computeDiffs({
+        previousData: result[0],
+        currentData: result[1],
+      });
     });
   }
 
+  computeDiffs({ previousData, currentData }) {
+    computeAndInjectDiffs(previousData, currentData);
+
+    this.setState({
+      width: Math.max(previousData[0].length, currentData[0].length),
+      height: previousData.length,
+    });
+
+    const worker = new ImageDiffWorker();
+    worker.addEventListener('message', (e) => {
+      const context = this.canvas.getContext('2d');
+      const diffImage = context.createImageData(
+        this.state.width, this.state.height);
+      e.data.forEach((value, index) => (diffImage.data[index] = value));
+      context.putImageData(diffImage, 0, 0);
+    });
+    worker.postMessage({ previousData, currentData });
+  }
+
   render() {
-    const { previousData, currentData } = this.state;
-
-    let maxWidth;
-    let maxHeight;
-
-    if (previousData && currentData) {
-      computeAndInjectDiffs(previousData, currentData);
-
-      maxWidth = Math.max(
-        previousData[0].length, currentData[0].length);
-      maxHeight = previousData.length;
-
-      const worker = new ImageDiffWorker();
-      worker.addEventListener('message', (e) => {
-        const context = this.canvas.getContext('2d');
-        const diffImage = context.createImageData(maxWidth, maxHeight);
-        e.data.forEach((value, index) => (diffImage.data[index] = value));
-        context.putImageData(diffImage, 0, 0);
-      });
-      worker.postMessage({ previousData, currentData });
-    }
+    const {
+      width,
+      height,
+    } = this.state;
 
     return (
       <canvas
-        width={maxWidth}
-        height={maxHeight}
         ref={(node) => { this.canvas = node; }}
+        width={width}
+        height={height}
       />
     );
   }
