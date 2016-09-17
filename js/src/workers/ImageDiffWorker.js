bbe2d35d38a4347f724acb5efa99d8cea3a2d1fc
Henric Trotzig <henric.trotzig@brigade.com>
2016-09-17 05:40:13 +0200

    Simplify ImageDiffWorker
    We don't have to worry about width and height here, we can just iterate
    over the pixels. This will simplify parallelizing multiple
    ImageDiffWorkers later on.

--- js/src/workers/ImageDiffWorker.js
+++ js/src/workers/ImageDiffWorker.js
@@ -1,22 +1,17 @@
 import getDiffPixel from '../getDiffPixel';
 
 self.addEventListener('message', ({ data: { previousData, currentData } }) => {
-  const data = new Uint8ClampedArray(
-    previousData.height * previousData.width * 4);
+  const data = new Uint8ClampedArray(previousData.data.length);
 
-  for (let y = 0; y < previousData.height; y++) {
-    for (let x = 0; x < previousData.width; x++) {
-      const index = (y * previousData.width * 4) + (x * 4);
-
-      const pixel = getDiffPixel(
-        previousData.data.slice(index, index + 4),
-        currentData.data.slice(index, index + 4)
-      );
-      data[index + 0] = pixel[0]; // r
-      data[index + 1] = pixel[1]; // g
-      data[index + 2] = pixel[2]; // b
-      data[index + 3] = pixel[3]; // a
-    }
+  for (let i = 0; i < previousData.data.length; i += 4) {
+    const pixel = getDiffPixel(
+      previousData.data.slice(i, i + 4),
+      currentData.data.slice(i, i + 4)
+    );
+    data[i + 0] = pixel[0]; // r
+    data[i + 1] = pixel[1]; // g
+    data[i + 2] = pixel[2]; // b
+    data[i + 3] = pixel[3]; // a
   }
   self.postMessage(data);
   self.close();
