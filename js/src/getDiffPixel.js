6e9e9fe670f0e711e957a0b627904a37026ec058
Joe Lencioni <joe.lencioni@gmail.com>
2016-09-18 11:04:08 -0700

    Render diffs on white background
    We've been tweaking the transparency of the rendered colors, which makes
    them a little hard to see and interpret. This commit brings in what we
    have been doing on the Ruby side by composing the colors on top of a
    white background.
    
    For this version, I did a straight up port of the functions we were
    using from ChunkyPNG. The performance considerations here might be
    different than they were in Rubyland, so we should look into any
    opportunities to optimize this code. Depending on what we learn here,
    there may also be some opportunities to optimize some other maths that
    we are doing, such as in euclideanDistance.

--- js/src/getDiffPixel.js
+++ js/src/getDiffPixel.js
@@ -1,4 +1,7 @@
+import compose from './compose';
 import euclideanDistance from './euclideanDistance';
+
+const WHITE = [255, 255, 255, 255];
 
 export default function getDiffPixel(previousPixel, currentPixel) {
   if (!previousPixel) {
@@ -11,13 +14,14 @@ export default function getDiffPixel(previousPixel, currentPixel) {
 
   const diff = euclideanDistance(previousPixel, currentPixel);
   if (diff === 0) {
-    return [
-      currentPixel[0],
-      currentPixel[1],
-      currentPixel[2],
-      50,
-    ];
+    return compose(
+      [currentPixel[0], currentPixel[1], currentPixel[2], 50],
+      WHITE
+    );
   }
 
-  return [255, 0, 0, 255 * Math.max(0.2, diff)]; // TODO don't use red here
+  return compose(
+    [255, 0, 0, 255 * Math.max(0.2, diff)], // TODO don't use red here
+    WHITE
+  );
 }
