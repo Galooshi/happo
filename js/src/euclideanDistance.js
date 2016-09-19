81a68bc86cbf03a7a3a217c850716a64a13a9d90
Joe Lencioni <joe.lencioni@gmail.com>
2016-09-18 19:55:33 -0700

    Move comment from euclideanDistance to proper location
    This comment doesn't actually apply to this function, so I'm moving it
    next to where it is appropriate.

--- js/src/euclideanDistance.js
+++ js/src/euclideanDistance.js
@@ -1,15 +1,3 @@
-/**
- * Compute a score that represents the difference between 2 pixels
- *
- * This method simply takes the Euclidean distance between the RGBA channels
- * of 2 colors over the maximum possible Euclidean distance. This gives us a
- * percentage of how different the two colors are.
- *
- * Although it would be more perceptually accurate to calculate a proper
- * Delta E in Lab colorspace, we probably don't need perceptual accuracy for
- * this application, and it is nice to avoid the overhead of converting RGBA
- * to Lab.
- */
 export default function euclideanDistance(rgba1, rgba2) {
   return Math.sqrt(
     Math.pow(rgba1[0] - rgba2[0], 2)
--- js/src/getDiffPixel.js
+++ js/src/getDiffPixel.js
@@ -12,7 +12,18 @@ export default function getDiffPixel(previousPixel, currentPixel) {
     return previousPixel;
   }
 
+  // Compute a score that represents the difference between 2 pixels
+  //
+  // This method simply takes the Euclidean distance between the RGBA channels
+  // of 2 colors over the maximum possible Euclidean distance. This gives us a
+  // percentage of how different the two colors are.
+  //
+  // Although it would be more perceptually accurate to calculate a proper
+  // Delta E in Lab colorspace, we probably don't need perceptual accuracy for
+  // this application, and it is nice to avoid the overhead of converting RGBA
+  // to Lab.
   const diff = euclideanDistance(previousPixel, currentPixel) / MAX_EUCLIDEAN_DISTANCE;
+
   if (diff === 0) {
     return compose(
       [currentPixel[0], currentPixel[1], currentPixel[2], 50],
