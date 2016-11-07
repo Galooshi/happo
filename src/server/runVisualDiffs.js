ef5b0f5eb1250b0b147b8cebd93f0cbf0313ad44
Henric Trotzig <henric.trotzig@brigade.com>
2016-11-07 08:19:49 +0100

    Increase script timeout
    I ran into some issues with scripts failing to resolve in the default
    script timeout (which was like 20ms or something). Setting the same
    timeout that we've had in the past, which seems to have been working
    well.

--- src/server/runVisualDiffs.js
+++ src/server/runVisualDiffs.js
@@ -14,7 +14,10 @@ const PNG = require('pngjs').PNG;
 function initializeDriver() {
   return new Promise((resolve, reject) => {
     try {
-      resolve(new seleniumWebdriver.Builder().forBrowser(config.driver).build());
+      const driver =
+        new seleniumWebdriver.Builder().forBrowser(config.driver).build();
+      driver.manage().timeouts().setScriptTimeout(3000);
+      resolve(driver);
     } catch (error) {
       reject(error);
     }
