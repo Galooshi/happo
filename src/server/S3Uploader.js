39eefc78241ef3923522ac4c589f12077c4d45b3
Joe Lencioni <joe.lencioni@airbnb.com>
2016-12-14 09:11:31 -0800

    Move S3 configuration into AWS.S3 initialization
    These should be equivalent, but I am running into permission problem
    when trying to upload to S3 in one project. The only thing that makes
    sense to me is that our configuration must not be getting wired up
    correctly. I've seen both styles of configuration, but since this one
    seems simpler and makes more sense to me, I'm going to give it a go.

--- src/server/S3Uploader.js
+++ src/server/S3Uploader.js
@@ -16,14 +16,12 @@ module.exports = class S3Uploader {
     this.debug = debug;
     this.debugLog(`Initializing S3 configuration for ${S3_ACCESS_KEY_ID}`);
 
-    AWS.config = new AWS.Config({
+    this.s3 = new AWS.S3({
       accessKeyId: S3_ACCESS_KEY_ID,
       secretAccessKey: S3_SECRET_ACCESS_KEY,
       region: S3_REGION || 'us-east-1',
       logger: debug && process.stderr,
     });
-
-    this.s3 = new AWS.S3();
 
     this.directory = [
       S3_BUCKET_PATH,
