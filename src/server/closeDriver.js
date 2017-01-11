341cab4dfc81fdbcb459636253a61d0f1266d869
Henric Trotzig <henric.trotzig@brigade.com>
2017-01-11 11:02:26 +0100

    Make `closeDriver` always resolve, never reject
    I was seeing this error when running happo in CentOS:
    
      UnhandledPromiseRejectionWarning: Unhandled promise rejection (rejection
      id: 1): Error: Command failed: ps l
    
    This was coming from src/server/closeDriver.js, which has the sole purpose
    of working around a bug in marionette [1]. By making it log the error
    while resolving the promise we can avoid the unhandled rejection. I'm
    not 100% sure why the `ps l` command fails, but looking at the processes
    on that machine I see no orphaned firefoxes running, so it's most likely
    safe to ignore.
    
    [1]: https://bugzilla.mozilla.org/show_bug.cgi?id=1310992

--- src/server/closeDriver.js
+++ src/server/closeDriver.js
@@ -12,25 +12,37 @@ function kill(pid) {
   });
 }
 
+function logError(err) {
+  console.warn(
+    'Saw error when making sure that firefox is closed properly. ' +
+    'This is most likely safe to ignore, but you might want to ' +
+    'manually check that no orphaned firefox processes are running.',
+  err);
+}
+
 // Works around an issue in marionette causing ghost firefox instances to remain
 // running.
 //
 // https://bugzilla.mozilla.org/show_bug.cgi?id=1310992
 // https://github.com/mozilla/geckodriver/issues/285
 module.exports = function closeDriver(driver) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     driver.close();
     psNode.lookup({
       command: 'firefox',
       arguments: '--marionette',
     }, (err, externalProcesses) => {
       if (err) {
-        reject(err);
+        logError(err);
+        resolve();
         return;
       }
       Promise.all(externalProcesses.map(({ pid }) => kill(pid)))
         .then(resolve)
-        .catch(reject);
+        .catch((error) => {
+          logError(error);
+          resolve();
+        });
     });
   });
 };
