8d918628ab08a6b5a2e77918a9ce462cbb79b444
Henric Trotzig <henric.trotzig@brigade.com>
2017-03-15 18:32:26 +0100

    Update import-js config for all packages
    To make import-js [1] work correctly, we need to configure it a little.
    
    Most of our packages are node projects using CommonJS imports and
    exports, so I created a root .importjs.js that package-specific config
    then inherited. These projects also transpile code into a `lib` folder,
    plus have test files in `__tests__` folders, so it was easy to set up
    the excludes configuration (which controls what files import-js won't
    import for you).
    
    The happo-viewer package was treated differently since it mostly has
    client-side React code where we use es6 imports and exports.
    
    [1]: https://github.com/Galooshi/import-js

--- packages/happo-target-react-native/src/setupServerAndWaitForSnapshots.js
+++ packages/happo-target-react-native/src/setupServerAndWaitForSnapshots.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const http = require('http');
+
+const express = require('express');
 const io = require('socket.io');
 
 function buildStoryIterator(socket, stories, onStorySnapshot, onFinished) {
