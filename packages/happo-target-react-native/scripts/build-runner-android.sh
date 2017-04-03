70ea5eabb2326746241b7987e4031ce232f54bfd
Spike Brehm <spike@airbnb.com>
2017-04-03 11:45:14 -0700

    [target-react-native] Fix comment in build-runner-android.sh

--- packages/happo-target-react-native/scripts/build-runner-android.sh
+++ packages/happo-target-react-native/scripts/build-runner-android.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Building HappoRunner.apk"
+echo "Building app-debug.apk"
 node ./node_modules/react-native/local-cli/cli.js run-android --root ./runner
 
-echo "Building HappoRunner.apk (done)"
+echo "Building app-debug.apk (done)"
