66939d4705d41dcd04c8726331cc8b1ab1e65dbf
Henric Trotzig <henric.trotzig@brigade.com>
2017-04-04 10:30:25 +0200

    Fix `happo debug firefox`
    There were two issues preventing the `happo debug firefox` from
    working:
    
    - A css file wasn't being injected into the ejs view
    - A bundled javascript file was missing
    
    I broke out some code from happo-viewer and moved it into the
    happo-target-firefox package.

--- packages/happo-target-firefox/src/prepareViewData.js
+++ packages/happo-target-firefox/src/prepareViewData.js
@@ -1,8 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
 const faviconAsBase64 = require('./faviconAsBase64');
+
+const CSS_FILE_PATH = path.join(__dirname, '../public/happo-styles.css');
+const JS_FILE_PATH = path.join(__dirname, '../public/HappoDebug.bundle.js');
 
 module.exports = function prepareViewData(data) {
   return {
     favicon: faviconAsBase64,
+    css: fs.readFileSync(CSS_FILE_PATH, 'utf8'),
+    jsCode: fs.readFileSync(JS_FILE_PATH, 'utf8'),
     ...data,
   };
 };
--- packages/happo-target-firefox/webpack.config.js
+++ packages/happo-target-firefox/webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   entry: {
     HappoRunner: './src/HappoRunner.js',
+    HappoDebug: './src/HappoDebug.jsx',
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
