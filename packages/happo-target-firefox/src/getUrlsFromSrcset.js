096af33da86c3d1c24141d1ed206e4011b73574f
Henric Trotzig <henric.trotzig@brigade.com>
2018-06-11 11:11:08 +0200

    Use string.prototype.matchall to grab urls from srcset
    As pointed out by @ljharb in a previous PR: https://github.com/Galooshi/happo/pull/224#pullrequestreview-127401748
    
    Multiple benefits here:
    - we avoid the while-loop
    - no mutation of the lastIndex property of the regexp

--- packages/happo-target-firefox/src/getUrlsFromSrcset.js
+++ packages/happo-target-firefox/src/getUrlsFromSrcset.js
@@ -1,10 +1,7 @@
+import matchAll from 'string.prototype.matchall';
+
 const SRCSET_ITEM = /([^\s]+)(\s+[0-9.]+[wx])?(,?\s*)/g;
 
 export default function getUrlsFromSrcset(value) {
-  const result = [];
-  let match;
-  while (match = SRCSET_ITEM.exec(value)) { // eslint-disable-line no-cond-assign
-    result.push(match[1]);
-  }
-  return result;
+  return Array.from(matchAll(value, SRCSET_ITEM)).map(groups => groups[1]);
 }
