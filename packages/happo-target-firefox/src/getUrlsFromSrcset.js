93c7b8b2c314deae2a105f70f3026751ece76865
Henric Trotzig <henric.trotzig@brigade.com>
2018-06-19 09:09:24 +0200

    Avoid intermediate array in getUrlsFromSrcset
    By using the second mapper argument we can avoid creating an
    intermediate array. As suggested by @ljharb:
    https://github.com/Galooshi/happo/pull/225#commitcomment-29321109

--- packages/happo-target-firefox/src/getUrlsFromSrcset.js
+++ packages/happo-target-firefox/src/getUrlsFromSrcset.js
@@ -3,5 +3,5 @@ import matchAll from 'string.prototype.matchall';
 const SRCSET_ITEM = /([^\s]+)(\s+[0-9.]+[wx])?(,?\s*)/g;
 
 export default function getUrlsFromSrcset(value) {
-  return Array.from(matchAll(value, SRCSET_ITEM)).map(groups => groups[1]);
+  return Array.from(matchAll(value, SRCSET_ITEM), groups => groups[1]);
 }
